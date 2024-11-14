acd24b5bbfc278eed054826e7ae97f05952d3de5
Sebastián Castillo <scastillo27@gmail.com>
2024-11-13 20:29:31 -0600

    Gifs Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+import { CardListComponent } from './gifs/components/card-list/card-list.component';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    GifsModule,
+    SharedModule,
+
+
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
